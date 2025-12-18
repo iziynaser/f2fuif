18dd8fc586d434e9d049638a13dbd40b546d4886
iziy <iziy_naser@yahoo.com>
2025-12-18 12:37:37 +0330

    حذف فایل های غیرلازم

--- src/router.js
+++ src/router.js
@@ -250,15 +250,6 @@ export default new Router({
       component: OrderSuccess,
       props: true//route => ({orderId:Number(route.params.orderId)})
     }
-    ,
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
   ]
 })
 
